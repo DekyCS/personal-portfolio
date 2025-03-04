c878d4b4a80898943bdb2006bef3bc3c21c55f41
Omar Lahlou Mimi <omarlahmimi@gmail.com>
2025-03-03 20:52:18 -0500

    add contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,7 +1,11 @@
 import Footer from "@/components/footer"
 import Navbar from "@/components/navbar"
 import { ThemeProvider } from "@/components/theme-provider"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
 import * as React from "react"
+import { SendHorizontal } from 'lucide-react';
 
 export default function Contact() {
     return (
@@ -12,7 +16,19 @@ export default function Contact() {
 
             <Navbar />
 
-            <h1 className='text-5xl calistoga-regular pt-8'>contact me.</h1>
+            <h1 className='text-5xl calistoga-regular py-8'>contact me.</h1>
+
+            <form className="flex flex-col gap-y-4">
+            <div className="flex flex-row gap-4 max-sm:flex-col">
+                <Input type="name" placeholder="Name" />
+                <Input type="email" placeholder="Email" />
+            </div>
+            <Textarea type="text" className="h-25 resize-none" placeholder="Leave feedback about the site, career opportunities or just to say hello etc." />
+
+            <Button type="submit" className="cursor-pointer">
+                Send Message <SendHorizontal />
+            </Button>
+            </form>
 
             {/* Footer */}
             
