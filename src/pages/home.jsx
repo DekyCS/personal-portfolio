a00b1a77caaf8ab529c80a86387538fe15c081fc
Omar Lahlou Mimi <54048627+DekyCS@users.noreply.github.com>
2025-07-16 10:51:49 -0400

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -34,6 +34,17 @@ export default function Home() {
 
     const jobs = [
         {
+          "imgSource": "https://i.pinimg.com/564x/b1/f1/70/b1f170bfba5817dd84429e5b5ebbcc85.jpg",
+          "time": "August 2025 - Present",
+          "company": "Apple",
+          "title": "Specialist",
+          "descriptions": [
+            "Provided tailored product recommendations and support to enhance customer experience in a fast-paced retail environment",
+            "Collaborated with team members to maintain an inclusive, knowledgeable, and solution-oriented store atmosphere"
+          ],
+          "links": []
+        },
+        {
           "imgSource": caeLogo,
           "time": "Summer 2025",
           "company": "CAE",
@@ -41,7 +52,8 @@ export default function Home() {
           "descriptions": [
             "Implemented DevOps pipelines and CI/CD automation",
             "Developed automated testing frameworks and scripts reduce manual testing"
-          ]
+          ],
+          "links": []
         },
         {
           "imgSource": "https://assets.grenier.qc.ca/cdn-cgi/image/q=90,format=webp,fit=pad,w=200,h=200/uploads/images/Logo-ITHQ-Marine-rgb-154839.png",
@@ -51,7 +63,8 @@ export default function Home() {
           "descriptions": [
             "Developed a Python desktop application with Tkinter for project organization and building a system to automatically sort and classify documents",
             "Built web scraping solutions using BeautifulSoup and APIs to analyze pet policies across 100+ hotels, creating reports on industry standards"
-          ]
+          ],
+          "links": []
         }
       ];
       
@@ -61,6 +74,7 @@ export default function Home() {
           "time": "August 2023 - May 2026",
           "company": "John Abbott College CEGEP",
           "title": "Computer Science Technical Program",
+          "descriptions": [],
           "links": []
         },
         {
@@ -68,6 +82,7 @@ export default function Home() {
           "time": "August 2022 - December 2022",
           "company": "Harvard",
           "title": "CS50 Introduction to Computer Science",
+          "descriptions": [],
           "links": [
               {
                   "name": "Certificate",
